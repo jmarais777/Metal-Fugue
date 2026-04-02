db74ec4ec7c65ed82d263719928e559e269f1fd0
jmarais777 <77393672+jmarais777@users.noreply.github.com>
2026-04-02 19:05:00 +0200

    Update PlayerMovement.cs

--- Metal Fugue/Assets/Scripts/Player/PlayerMovement.cs
+++ Metal Fugue/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -5,18 +6,21 @@ public class PlayerMovement : MonoBehaviour
     public float movementSpeed = 5f;
     public Rigidbody2D rigBod;
     Vector2 movement;
-    
-    //player movement
+
+    //Player Movement
+    //Title: Top Down Movement In UNITY 6
+    //Author: Unity Unlocked
+    //Date: 02/04/2026
+    //Availiability: https://www.youtube.com/watch?v=Rs8Wy9jH8iA
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
     }
 
+ 
     private void FixedUpdate()
     {
         rigBod.MovePosition(rigBod.position + movement.normalized * movementSpeed * Time.deltaTime);
     }
-
-    //dodge
 }
